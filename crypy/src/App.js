b7c389fa13d1690f3b9fa2cb2e2fba181acef750
Francis Kim <franciskim722@gmail.com>
2017-09-15 02:23:50 -0700

    + Render table

--- crypy/src/App.js
+++ crypy/src/App.js
@@ -15,54 +15,66 @@ class App extends Component {
     super(props);
     this.state = {
       socketState: '',
+      marketHeaders: [],
       marketLive: [],
       marketSummary:[]
-    }
+    };
+    this.renderCell = this.renderCell.bind(this);
   }
 
-  componentDidMount(){
+  componentWillMount(){
     axios.get(`http://localhost:4000`)
     .then((res) => {
-      const marketSummary = res.data.result;
-      this.setState({marketSummary});
+      this.setState({
+        marketHeaders: Object.keys(res.data.result[0]),
+        marketSummary:res.data.result
+      });
     });
 
-    socket.on('connect', () => {
-      console.log("Connected to socket");
-    });
+    // socket.on('connect', () => {
+    //   console.log("Connected to socket");
+    // });
+    //
+    // socket.on('receiveBittrex', (data) => {
+    //   if (Object.keys(data).length === 0 && data.constructor === Object) {
+    //   } else {
+    //     let marketLive = data.M[0].A[0].Deltas;
+    //     this.setState({marketLive});
+    //   }
+    // });
+    //
+    // socket.emit('getBittrex');
+    // socket.on('disconnect', function(){});
+  }
 
-    socket.on('receiveBittrex', (data) => {
-      if (Object.keys(data).length === 0 && data.constructor === Object) {
-      } else {
-        let marketLive = data.M[0].A[0].Deltas;
-        this.setState({marketLive});
-      }
-    });
-
-    socket.emit('getBittrex');
-    socket.on('disconnect', function(){});
+  renderCell(rowIndex, colIndex){
+    const {marketHeaders, marketSummary} = this.state;
+    return (
+      <Cell>
+        {marketSummary[rowIndex][marketHeaders[colIndex]]}
+      </Cell>
+    )
   }
 
   render() {
-    const { marketLive, marketSummary } = this.state;
+    const { marketHeaders, marketSummary } = this.state;
 
-    if(marketLive){
       return (
-        <div className="App">
-          <Table numRow={5}>
-            <Column name="Coin Information"/>
-            <Column />
-            <Column />
+        <div className="pt-card pt-elevation-4 crypy-card">
+          <h2>Market Information:</h2>
+          <Table numRows={marketSummary.length}>
+            {marketHeaders.map((col, i) => {
+              return (
+                <Column
+                  key={i}
+                  name={col}
+                  renderCell={this.renderCell}
+                />
+              );
+            })}
           </Table>
         </div>
       );
-    } else {
-      return (
-        <div>
-          Loading...
-        </div>
-      )
-    }
   }
 }
 
